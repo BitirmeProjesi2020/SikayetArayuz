92def7faeb6164b760bee89d50020c76c9a46b73
Nur Sultan Bolel <nursultanbolel@gmail.com>
2020-05-20 19:58:23 +0300

    navbar degisti

--- src/app/navbarcustom/navbarcustom.component.ts
+++ src/app/navbarcustom/navbarcustom.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbarcustom',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarcustomComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  logIn() {
+    this.router.navigate(['/uye-girisi']);
+  }
+  signUp() {
+    this.router.navigate(['/uye-ol']);
+  }
+
 
 }
