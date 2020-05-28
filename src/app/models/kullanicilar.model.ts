baf74e7a179041031d129c9b266f8fe7fff8f2a5
Nur Sultan Bolel <nursultanbolel@gmail.com>
2020-05-28 22:21:37 +0300

    signUp islemi eklendi

--- src/app/models/kullanicilar.model.ts
+++ src/app/models/kullanicilar.model.ts
@@ -3,4 +3,13 @@ export class Kullanicilar {
   adSoyad: string;
   email: string;
   password: string;
+
+  constructor(id, adSoyad: string, email: string, password: string) {
+    this.id = id;
+    this.adSoyad = adSoyad;
+    this.email = email;
+    this.password = password;
+  }
+
+
 }
--- src/app/signupcustomer/signupcustomer.component.ts
+++ src/app/signupcustomer/signupcustomer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Kullanicilar } from '../models/kullanicilar.model';
+import { KullanicilarService } from '../services/kullanicilar.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signupcustomer',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signupcustomer.component.css']
 })
 export class SignupcustomerComponent implements OnInit {
+  alertText: string = "";
+  email: string;
+  kullanici: Kullanicilar;
+  password: string;
+  passwordTkr: string;
+  username: string;
 
-  constructor() { }
+  constructor(private kullanicilarService: KullanicilarService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  signUpBtn() {
+    if (this.username == undefined || this.email == undefined || this.password == undefined) {
+      this.alertText = "Tüm alanları doldurunuz!";
+    }
+    else if (this.password != this.passwordTkr) {
+      this.alertText = "Şifrenizi kontrol edip tekrar deneyin!";
+    }
+    else {
+      this.kullanici = { id: null, adSoyad: this.username, email: this.email, password: this.password }
+
+      this.kullanicilarService.add(this.kullanici).pipe().subscribe((data) => {
+        this.alertText = "Kayıt işlemi başarılı.";
+        this.router.navigate(['/']);
+      });
+    }
+  }
 }
