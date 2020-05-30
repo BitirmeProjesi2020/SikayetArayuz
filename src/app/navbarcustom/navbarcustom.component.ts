17bbe993f07b4828bd57f0738436378ef499ca04
Nur Sultan Bolel <nursultanbolel@gmail.com>
2020-05-30 14:58:10 +0300

    navbar guncellendi
    cookie bilgileri konrol edilerek profil menusu eklendi.

--- src/app/navbarcustom/navbarcustom.component.ts
+++ src/app/navbarcustom/navbarcustom.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,17 +9,42 @@ import { Router } from '@angular/router';
 })
 export class NavbarcustomComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  kullanici: boolean;
+  kullaniciName: string;
+  constructor(private cookieService: CookieService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.controlCookie();
+  }
+
+  controlCookie() {
+    console.log (this.kullanici);
+    if(this.cookieService.get('kullaniciId') === ""){
+      this.kullanici = false;
+      console.log("Kullanici giriş yapmamış.");
+    }
+    else{
+      this.kullanici = true;
+      this.kullaniciName = this.cookieService.get('kullaniciAdSoyad');
+      console.log("Kullanici giriş yapmıştır.");
+    }
+  }
+
+  deleteCookie(){
+    this.cookieService.delete('kullaniciId');
+    this.cookieService.delete('kullaniciAdSoyad');
+    this.cookieService.delete('kullaniciEmail');
+    this.cookieService.delete('kullaniciPassword');
+    this.ngOnInit();
   }
 
   logIn() {
     this.router.navigate(['/uye-girisi']);
   }
+  
   signUp() {
     this.router.navigate(['/uye-ol']);
   }
-
 
 }
