08e212614e21f73df1c7f897f71bac811c3a1881
serifercanozkan <39194386+serifercanozkan@users.noreply.github.com>
2020-05-31 16:23:28 +0300

    Calisan için üye girişi yapıldı

--- src/app/logincustomer/logincustomer.component.ts
+++ src/app/logincustomer/logincustomer.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
-import { Kullanicilar } from '../models/kullanicilar.model';
-import { KullanicilarService } from '../services/kullanicilar.service';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {CookieService} from 'ngx-cookie-service';
+import {Kullanicilar} from '../models/kullanicilar.model';
+import {KullanicilarService} from '../services/kullanicilar.service';
+import {Router} from '@angular/router';
+import {BankaCalisanlari} from '../models/bankaCalisanlari.model';
+import {BankaCalisanlariService} from '../services/bankaCalisanlari.service';
 
 @Component({
   selector: 'app-logincustomer',
@@ -11,22 +13,32 @@ import { Router } from '@angular/router';
 })
 export class LogincustomerComponent implements OnInit {
 
-  alertText: string = "";
+  alertText: string = '';
   email: string;
   flag: boolean = false;
   password: string;
   kullanicilarList: Kullanicilar[];
+  bankaCalisanlariList: BankaCalisanlari[];
 
   constructor(private cookieService: CookieService,
               private kullanicilarService: KullanicilarService,
-              private router: Router) { }
+              private bankaCalisanlariService: BankaCalisanlariService,
+              private router: Router) {
+  }
 
   ngOnInit(): void {
     this.getKullanicilar();
+    this.getBankaCalisanlari();
   }
 
   alertMsg(msg: string) {
     this.alertText = msg;
+  }
+
+  getBankaCalisanlari() {
+    this.bankaCalisanlariService.getAll().pipe().subscribe((data: BankaCalisanlari[]) => {
+      this.bankaCalisanlariList = data;
+    });
   }
 
   getKullanicilar() {
@@ -41,21 +53,27 @@ export class LogincustomerComponent implements OnInit {
     if (this.email != undefined && this.password != undefined) {
       this.kullanicilarList.forEach(kullanici => {
         if (this.email === kullanici.email && this.password == kullanici.password) {
-          this.cookieService.set('kullaniciId', kullanici.id.toString());
-          this.cookieService.set('kullaniciAdSoyad', kullanici.adSoyad);
-          this.cookieService.set('kullaniciEmail', kullanici.email);
-          this.cookieService.set('kullaniciPassword', kullanici.password);
+          this.cookieService.set('uyeId', kullanici.id.toString());
+          this.cookieService.set('uyeTipi', 'musteri');
           this.flag = true;
-          this.alertMsg("Giriş işlemi başarılı.");
+          this.alertMsg('Giriş işlemi başarılı.');
+          this.router.navigate(['']);
+        }
+      });
+      this.bankaCalisanlariList.forEach(calisan => {
+        if (this.email === calisan.email && this.password == calisan.password) {
+          this.cookieService.set('uyeId', calisan.id.toString());
+          this.cookieService.set('uyeTipi', 'calisan');
+          this.flag = true;
+          this.alertMsg('Giriş işlemi başarılı.');
           this.router.navigate(['']);
         }
       });
       if (!this.flag) {
-        this.alertMsg("E-mail veya şifreyi yanlış girdiniz. Tekrar deneyiniz.");
+        this.alertMsg('E-mail veya şifreyi yanlış girdiniz. Tekrar deneyiniz.');
       }
-    }
-    else {
-      this.alertMsg("E-maail ve şifre alanlarını doldurunuz.");
+    } else {
+      this.alertMsg('E-maail ve şifre alanlarını doldurunuz.');
     }
   }
 
