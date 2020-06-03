824d6df5ebf67827a77d57149e60287a69239692
Nur Sultan Bolel <nursultanbolel@gmail.com>
2020-06-03 14:48:50 +0300

    navbar guncellendi
    Menu isimleri stili degisti, username eklendi

--- src/app/navbarcustom/navbarcustom.component.ts
+++ src/app/navbarcustom/navbarcustom.component.ts
@@ -1,5 +1,9 @@
+import {BankaCalisanlari} from '../models/bankaCalisanlari.model';
+import {BankaCalisanlariService} from '../services/bankaCalisanlari.service';
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { Kullanicilar } from '../models/kullanicilar.model';
+import { KullanicilarService } from '../services/kullanicilar.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,7 +14,10 @@ import { Router } from '@angular/router';
 export class NavbarcustomComponent implements OnInit {
 
   kullanici: boolean;
-  constructor(private cookieService: CookieService,
+  kullaniciName: string;
+  constructor( private bankaCalisanlariService:BankaCalisanlariService,
+              private cookieService: CookieService,
+              private kullanicilarService: KullanicilarService,
               private router: Router) { }
 
   ngOnInit(): void {
@@ -18,29 +25,45 @@ export class NavbarcustomComponent implements OnInit {
   }
 
   controlCookie() {
-    if(this.cookieService.get('uyeId') === ""){
+    if (this.cookieService.get('uyeId') === "") {
       this.kullanici = false;
-      console.log("Calisan veya Musteri giriş yapmamış.");
     }
-    else{
+    else {
       this.kullanici = true;
-      console.log("Calisan veya Musteri giris yapmıştır.");
-      console.log(this.cookieService.get('uyeTipi'));
+      if(this.cookieService.get('uyeTipi') === "musteri"){
+        this.getMusteriName();
+      }
+      else{
+        this.getCalisanName();
+      }
     }
   }
 
-  deleteCookie(){
+  deleteCookie() {
     this.cookieService.delete('uyeId');
     this.cookieService.delete('uyeTipi');
     this.router.navigate(['']);
+    this.ngOnInit();
   }
 
   logIn() {
     this.router.navigate(['/uye-girisi']);
   }
-  
+
   signUp() {
     this.router.navigate(['/uye-ol']);
+  }
+
+  getMusteriName() {
+    this.kullanicilarService.getById(Number(this.cookieService.get('uyeId'))).pipe().subscribe((data: Kullanicilar) => {
+      this.kullaniciName = data.adSoyad;
+    });
+  }
+
+  getCalisanName(){
+    this.bankaCalisanlariService.getById(Number(this.cookieService.get('uyeId'))).pipe().subscribe((data: BankaCalisanlari) => {
+      this.kullaniciName = data.adSoyad; 
+    });
   }
 
 }
