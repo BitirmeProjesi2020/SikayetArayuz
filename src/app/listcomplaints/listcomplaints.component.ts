a8de45fbc3c52ed840f0dba8d84d1c45ef3880aa
Nur Sultan Bolel <nursultanbolel@gmail.com>
2020-06-05 21:14:42 +0300

    sikayet filtreleme deneme

--- src/app/listcomplaints/listcomplaints.component.ts
+++ src/app/listcomplaints/listcomplaints.component.ts
@@ -22,6 +22,11 @@ export class ListcomplaintsComponent implements OnInit {
   kullanicilarList: Kullanicilar[];
   sikayetlerList: Sikayetler[];
   kategorilerList: Kategoriler[];
+  selectedBankalar: boolean[] = new Array();
+  selectedKategoriler: boolean[] = new Array();
+  eslesenBanka: Sikayetler[] = new Array();
+  eslesenKategori: Sikayetler[] = new Array();
+  eslesenSikayetler: Sikayetler[] = new Array();
 
   constructor(private router: Router,
               private cookieService: CookieService,
@@ -48,6 +53,84 @@ export class ListcomplaintsComponent implements OnInit {
     } else {
       this.calisan = false;
     }
+  }
+
+  filterBank(id){
+    this.eslesenBanka.length = 0;
+    this.eslesenKategori.length = 0;
+    this.eslesenSikayetler.length = 0;
+    id = id - 1;
+    if(this.selectedBankalar[id] === false || this.selectedBankalar[id] === undefined){
+      this.selectedBankalar[id] = true;
+    }
+    else{
+      this.selectedBankalar[id] = false;
+    }
+    console.log('secilen bankalar: ', this.selectedBankalar);
+    console.log('secilen kategoriler: ', this.selectedKategoriler);
+    this.getEslesen();
+  }
+
+  filterKategori(id){
+    this.eslesenKategori.length = 0; 
+    id = id - 1;
+    if(this.selectedKategoriler[id] === false || this.selectedKategoriler[id] === undefined){
+      this.selectedKategoriler[id] = true;
+    }
+    else{
+      this.selectedKategoriler[id] = false;
+    }
+    console.log('secilen bankalar: ', this.selectedBankalar);
+    console.log('secilen kategoriler: ', this.selectedKategoriler);
+    this.getEslesen();
+  }
+
+  getEslesen(){
+    for(let i=0; i< this.sikayetlerList.length ; i++){
+      for(let j=0; j< this.selectedBankalar.length ; j++){
+        if(this.selectedBankalar[j]===true && this.bankalarList[j].ad === this.sikayetlerList[i].bankaId){
+          this.eslesenBanka.push(this.sikayetlerList[i]);
+        }
+      }
+    }
+    for(let i=0; i< this.sikayetlerList.length ; i++){
+      for(let j=0; j< this.selectedKategoriler.length ; j++){
+        if(this.selectedKategoriler[j]===true && this.kategorilerList[j].id === this.sikayetlerList[i].sikayetKategorisi){
+          this.eslesenKategori.push(this.sikayetlerList[i]);
+        }
+      }
+    }
+    console.log('eslesen bankalar: ',this.eslesenBanka);
+    console.log('eslesen kategoriler: ',this.eslesenKategori);
+    this.getEslesenSikayetler();
+  }
+
+  getEslesenSikayetler(){
+    console.log('Eslesen Sikayetler:')
+    this.eslesenSikayetler.length = 0;
+    console.log('eslesenBankaLength: ', this.eslesenBanka.length);
+    console.log('eslesenKategoriLength: ', this.eslesenKategori.length);
+    if(this.eslesenBanka.length === 0 && this.eslesenKategori.length != 0){
+      this.eslesenSikayetler = this.eslesenKategori;
+      console.log('banka bos: ',this.eslesenSikayetler);
+      return;
+    }
+    if(this.eslesenKategori.length === 0 && this.eslesenBanka.length != 0){
+      this.eslesenSikayetler = this.eslesenBanka;
+      console.log('kategori bos: ',this.eslesenSikayetler);
+      return;
+    }
+    if(this.eslesenBanka.length != 0 && this.eslesenKategori.length != 0){
+      console.log('banka-kategori join: ');
+      for(let i=0; i< this.eslesenBanka.length ; i++){
+        for(let j=0; j< this.eslesenKategori.length ; j++){
+          if(this.eslesenBanka[i].id === this.eslesenKategori[j].id){
+            this.eslesenSikayetler.push(this.eslesenKategori[i]);
+          }
+        }
+      }
+    }
+    
   }
 
   getBankalar() {
